fba5cb9cdd63560c3164e254834f9b97086300cf
RishiNair1110 <Nairrishi2001@gmail.com>
2022-02-12 15:05:29 +0300

    files updated

--- naari-safety/src/App.js
+++ naari-safety/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='loginbox'>
+        <input
+          className="email"
+          type="text"
+          placeholder='Enter Email Address'
+        />
+        <input
+          className="pass"
+          type="password"
+          placeholder='Enter your password'
+        />
+        <button className="btn">Log In</button>
+      </div>
     </div>
   );
 }
