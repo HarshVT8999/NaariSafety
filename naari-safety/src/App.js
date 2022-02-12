ed74ec359bca6b06322a04fa6ab7cf14d64d8997
Harsh <harshvardhantiwari@Harshvardhans-MacBook-Air.local>
2022-02-12 18:53:00 +0530

    files updated

--- naari-safety/src/App.js
+++ naari-safety/src/App.js
@@ -3,17 +3,26 @@ function App() {
   return (
     <div className="App">
       <div className='loginbox'>
+        <form>
+        <h3>Login Here</h3>
+        <label for="email">Username</label>
         <input
           className="email"
           type="text"
-          placeholder='Enter Email Address'
+          placeholder='Email or Phone'
         />
+        <label for="pass">Password</label>
         <input
           className="pass"
           type="password"
-          placeholder='Enter your password'
+          placeholder='Password'
         />
         <button className="btn">Log In</button>
+        <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+        </form>
       </div>
     </div>
   );
